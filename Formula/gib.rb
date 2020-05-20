786ae0223423e05e3304112279435c6bbce0d460
David Sánchez <d.vinternatt@gmail.com>
2020-05-20 21:22:55 +0100

    Add SHA256 for gib-v0.2.2

--- Formula/gib.rb
+++ Formula/gib.rb
@@ -1,8 +1,8 @@
 class Gib < Formula
   desc ".gitignore bootstrapper for projects that use git"
   homepage "https://github.com/DavSanchez/gib"
-  url "https://github.com/DavSanchez/gib/archive/v0.2.1.tar.gz"
-  sha256 "58c7573aa4ddcee3702fa6982ff9b4ef11e06751459421f97c0c625b6c75473c"
+  url "https://github.com/DavSanchez/gib/archive/v0.2.2.tar.gz"
+  sha256 "77587c299ddd62e40e048ef54926ea1c628f9517eb6b3069f963a1c6e84acb9f"
   head "https://github.com/Davsanchez/gib.git"
 
   depends_on "rust" => :build
