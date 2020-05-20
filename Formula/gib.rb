23a3581366396731820ee845a1f9f10bfe1fd101
David Sánchez <d.vinternatt@gmail.com>
2020-05-20 23:51:01 +0100

    Modify installation process

--- Formula/gib.rb
+++ Formula/gib.rb
@@ -1,14 +1,13 @@
 class Gib < Formula
   desc ".gitignore bootstrapper for projects that use git"
   homepage "https://github.com/DavSanchez/gib"
-  url "https://github.com/DavSanchez/gib/archive/v0.2.2.tar.gz"
-  sha256 "77587c299ddd62e40e048ef54926ea1c628f9517eb6b3069f963a1c6e84acb9f"
+  url "https://github.com/DavSanchez/gib.git", :tag => "v0.2.2"
   head "https://github.com/Davsanchez/gib.git"
 
   depends_on "rust" => :build
 
   def install
-    system "git", "clone", "https://github.com/github/gitignore.git"
+    system "git", "submodule", "update", "--init"
     system "cargo", "install", "--locked", "--root", prefix, "--path", "."
   end
 
