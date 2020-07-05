64ca2d94f00c0d8d04394646457a111af360db21
David Sánchez <d.vinternatt@gmail.com>
2020-07-05 22:56:02 +0100

    gib v0.2.4

--- Formula/gib.rb
+++ Formula/gib.rb
@@ -1,7 +1,7 @@
 class Gib < Formula
   desc ".gitignore bootstrapper for projects that use git"
   homepage "https://github.com/DavSanchez/gib"
-  url "https://github.com/DavSanchez/gib.git", :tag => "v0.2.3"
+  url "https://github.com/DavSanchez/gib.git", :tag => "v0.2.4"
   head "https://github.com/Davsanchez/gib.git"
 
   depends_on "rust" => :build
